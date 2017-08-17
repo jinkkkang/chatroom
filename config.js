4fde5481b40bf485765b303b245f6cf4065f8d62
jkwin <jkk@qq.com>
2017-08-17 18:08:50 +0800

    add

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-	mongodb:'mongodb://47.93.80.245:27017/chat',
+	mongodb:'mongodb://*/chat',
 	session:{
         name: "kangchat",
         secret: "kangchat", 
         maxAge: 2592000000
 	},
 	mail:{
-		name: "1547182170@qq.com",
+		name: "*",
 		mailhost: "smtp.qq.com",
-		password: "kbvabbugsdythhga"
+		password: "*"
 	}
 
 }
